f0907d8a3146f901621a462f50cfba0e49368f1e
Anderson G. A. Alves <andersongaalves1@gmail.com>
2026-09-07 09:40:58 -0300

    fix: allow Cloudflare frontend origin

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://dark-district-shop.andersongaalves1.workers.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
